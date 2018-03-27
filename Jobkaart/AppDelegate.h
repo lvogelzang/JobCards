18647fe44745963c9c65f9bb702550693d3ccb1e
Lodewijck <lodewijck@Lodewijcks-MacBook-Pro.local>
2018-03-27 20:52:04 +0200

    Initial Commit

---
+++ Jobkaart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jobkaart
+//
+//  Created by Lodewijck on 27/03/2018.
+//  Copyright © 2018 Lodewijck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JobkaartTests/JobkaartTests.m
@@ -0,0 +1,39 @@
+//
+//  JobkaartTests.m
+//  JobkaartTests
+//
+//  Created by Lodewijck on 27/03/2018.
+//  Copyright © 2018 Lodewijck. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JobkaartTests : XCTestCase
+
+@end
+
+@implementation JobkaartTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
